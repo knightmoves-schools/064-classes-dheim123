745291ee91dab2b8833ad05d520899a033630e90
dheim123 <dheim@midland.k12.ia.us>
2025-11-24 12:20:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
 
+const guests = [
+  new Person("John", "Rickels"),
+  new Person("Jane", "Smith"),
+  new Person("Bob", "Stone")
+];
